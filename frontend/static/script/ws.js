e3173e15c29e2d4d54aebd994347329ed470bb61
Alan Panayotov <alanpanayotov@gmail.com>
2021-04-18 19:43:36 +0100

    support binary base64 transport

--- frontend/static/script/ws.js
+++ frontend/static/script/ws.js
@@ -9,5 +9,18 @@ socket.addEventListener('open', function (event) {
 
 // Listen for messages
 socket.addEventListener('message', function (event) {
-    serverConsole.addLine(event.data);
+    if(typeof event.data == "string") { // plaintext resp, must be from rcon
+        serverConsole.addLine(event.data);
+    } else { // binary response, must be from communicator itself
+        event.data.arrayBuffer().then((buf) => {
+            const view = new Uint8Array(buf);
+            // TODO: this can definitely be optimised
+            let str = "";
+            for(let i = 0; i < view.length; i++) {
+                str += String.fromCharCode(view[i]);
+            }
+            console.log(view);
+            console.log(atob(str));
+        });
+    }
 });
