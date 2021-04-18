34e705a952f1669253da57c263ac23460fd15bdf
Alan Panayotov <alanpanayotov@gmail.com>
2021-04-18 18:46:24 +0100

    add communicator struct

---
+++ websocket/src/communicator.rs
@@ -0,0 +1,11 @@
+use std::net::ToSocketAddrs;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait Communicator {
+  fn name(&self) -> String;
+  fn set_name(&mut self, name: String);
+
+  async fn send_cmd(&mut self, cmd: String) -> String;
+  async fn connect(&mut self, address: &str, password: &str) -> Result<(), rcon::Error>;
+}
