7027ed81cf003a1fb0f81c91b1d4a3399a46169a
Alan Panayotov <alanpanayotov@gmail.com>
2021-04-19 02:02:43 +0100

    clientside status command

--- frontend/static/script/server.js
+++ frontend/static/script/server.js
@@ -21,6 +21,11 @@ conn.oncmd = (cmd) => {
       serverConsole.addLine(cmd.body, "meta");
     break;
 
+    case "Status":
+      stats.stats = cmd.body;
+      stats.updateStats();
+    break;
+
     default:
       console.error("unexpected command received:")
       console.error(cmd);
