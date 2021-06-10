b71f6bea613d7f89ef5e099f8b0eb44fe8a12424
Alan Panayotov <alanpanayotov@gmail.com>
2021-06-10 20:55:51 +0100

    wait how long was this missing

---
+++ websocket/src/commands/mod.rs
@@ -0,0 +1,75 @@
+use crate::{communicators::CommunicatorType, server::{Message, ServerInfo}, state::Client};
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+
+mod command;
+mod create_server;
+mod list_servers;
+mod remove_server;
+mod server_log;
+mod set_server;
+mod status;
+mod update_server;
+
+pub use {
+  command::command,
+  create_server::create_server,
+  list_servers::list_servers,
+  remove_server::remove_server,
+  server_log::server_log,
+  set_server::set_server,
+  status::status,
+  update_server::update_server,
+};
+use crate::state::State;
+use tokio::sync::RwLock;
+use futures::channel::mpsc::UnboundedSender;
+
+#[derive(Debug)]
+#[derive(Serialize)]
+#[serde(tag = "type", content = "body")]
+pub enum ServerCommand {
+  Status(ServerInfo),
+  Print(String),
+  ForeignCommand{id: Uuid, cmd: String, out: String},
+  Identity(Client),
+
+  ServerLog{page_no: usize, messages: Vec<Message>, server_id: Uuid},
+
+  ServerList(Vec<ServerInfo>)
+}
+
+#[derive(Debug)]
+#[derive(Deserialize)]
+#[serde(tag = "type", content = "body")]
+pub enum ClientCommand {
+  Command{id: Uuid, cmd: String},
+  Status(Option<Uuid>), // get status of server
+
+  SetServer(Uuid), // set client's current server id
+  ServerLog(Option<usize>), // get page of server log (if no page specified, return last page)
+
+  CreateServer,
+  UpdateServer{id: Uuid, name: Option<String>, communicator_type: Option<CommunicatorType>},
+  RemoveServer(Uuid),
+  ListServers,
+}
+
+pub async fn process_command(cmd: ClientCommand, client_id: &Uuid, state: &RwLock<State>) -> Option<tungstenite::Message>{
+  match cmd {
+    ClientCommand::Command{id, cmd} => command(state, client_id, &id, &cmd).await,
+    ClientCommand::Status(id) => status(state, client_id, &id).await,
+
+    ClientCommand::SetServer(id) => set_server(state, client_id, &id).await,
+    ClientCommand::ServerLog(page_no) => server_log(state, client_id, &page_no).await,
+
+    ClientCommand::CreateServer => create_server(state, client_id).await,
+    ClientCommand::UpdateServer {
+      id,
+      name,
+      communicator_type
+    } => update_server(state, client_id, &id, &name, &communicator_type).await,
+    ClientCommand::RemoveServer(id) => remove_server(state, client_id, &id).await,
+    ClientCommand::ListServers => list_servers(state, client_id).await,
+  }
+}
