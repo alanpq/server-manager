7050cee250e59955efb261e09d87274032169dfa
Alan Panayotov <alanpanayotov@gmail.com>
2021-06-11 09:30:41 +0100

    server console basics

--- app/src/components/ServerConsole.tsx
+++ app/src/components/ServerConsole.tsx
@@ -2,8 +2,25 @@ import React from "react";
 
 import './ServerConsole.scss'
 
-export function ServerConsole() {
-  return <section className="console">
+function Line() {
+  return <li>
+    <span className="timestamp">08:46</span>
+    <span className="body">{Math.random()}</span>
+  </li>
+}
 
+export function ServerConsole() {
+
+  return <section className="console">
+    <div>
+      <ul>
+        {
+          new Array<boolean>(30).fill(false).map((value, idx) => {
+            return <Line />
+          })
+        }
+      </ul>
+    </div>
+    <input/>
   </section>;
 }
