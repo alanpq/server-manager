0f5443db7765afb81a1fffb09914262c1420266e
Alan Panayotov <alanpanayotov@gmail.com>
2021-06-11 05:43:01 +0100

    add react router / separate Dashboard as component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,13 +6,37 @@ import {ServerDetails} from "./components/ServerDetails";
 import {ServerConsole} from "./components/ServerConsole";
 import {ServerList} from "./components/ServerList";
 import {ServerTabs} from "./components/ServerTabs";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function Dashboard(props: {
+  onOpen: (server: Server) => void,
+  servers: { [p: string]: Server },
+}) {
+  const [server, setServer]: [Server | null, any] = useState(null);
+  return <main>
+    <ServerList
+      onOpen={props.onOpen}
+      onChange={(id) => {
+        setServer(props.servers[id]);
+        console.log(id);
+      }}
+    />
+    <ServerDetails server={server}/>
+    <article className="mini-console">
+      <ServerConsole/>
+    </article>
+  </main>;
+}
 
 function App() {
   const [servers, setServers]: [{[name: string]: Server}, any] = useState({});
   const [tabs, setTabs]: [string[], any] = useState([]);
   const [tabIdx, setTabIdx] = useState(-1);
 
-  const [server, setServer]: [Server | null, any] = useState(null);
 
   const list = useServerList();
 
@@ -26,29 +50,27 @@ function App() {
   return (
     <>
       <header>
-        <ServerTabs tabs={tabs} servers={servers} curTab={tabIdx} onChange={(new_tab) => {setTabIdx(new_tab)}}/>
+        <ServerTabs tabs={tabs} servers={servers} curTab={tabIdx} onChange={(new_tab) => {
+          setTabIdx(new_tab)
+        }}/>
       </header>
-      <main>
-        <ServerList
-          onOpen={(srv) => {
-            const idx = tabs.indexOf(srv.id);
-            if (idx !== -1) {
-              setTabIdx(idx);
-            } else {
-              setTabIdx(tabs.length);
-              setTabs(tabs.concat(srv.id))
-            }
-          }}
-          onChange={(id) => {
-            setServer(servers[id]);
-            console.log(id);
-          }}
-        />
-        <ServerDetails server={server}/>
-        <article className="mini-console">
-          <ServerConsole />
-        </article>
-      </main>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Dashboard servers={servers}
+              onOpen={(srv) => {
+                const idx = tabs.indexOf(srv.id);
+                if (idx !== -1) {
+                  setTabIdx(idx);
+                } else {
+                  setTabIdx(tabs.length);
+                  setTabs(tabs.concat(srv.id))
+                }
+              }}
+            />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
